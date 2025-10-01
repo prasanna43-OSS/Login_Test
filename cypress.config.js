15237821050588c7cf18ce533a3c35e4bfb9e209
prasanna <prasanna4344@gmail.com>
2025-10-02 01:35:55 +0530

    remove report

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,9 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // register the reporter plugin
-      const mochawesome = require('cypress-mochawesome-reporter/plugin');
-      mochawesome(on);
-      return config;
+      // implement node event listeners here
     },
-    baseUrl: 'https://example.com',  // replace with your AUT
-  },
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    reportDir: 'cypress/reports',
-    charts: true,
-    overwrite: false,
-    html: true,
-    json: true,
   },
 });
